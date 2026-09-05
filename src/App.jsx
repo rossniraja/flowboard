fed74f4732ec3e10f9d95fbc720cca3603f68f88
Rossni R <rossniraja@gmail.com>
2026-09-05 18:55:35 +0530

    Fix component import casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import CalendarView from "./components/CalendarView";
 import ListView from "./components/ListView";
-import Board from "./components/board";
-import AddTaskForm from "./components/addTaskForm";
+import Board from "./components/Board";
+import AddTaskForm from "./components/AddTaskForm";
 import "./app.css";
 import { useState } from "react";
 
